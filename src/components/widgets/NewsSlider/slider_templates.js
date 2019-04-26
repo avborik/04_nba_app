2d7c4da93e2cc5828d99a6fa3ca74395611ac0b9
AvBorikDeveloper <avboris28@gmail.com>
2019-04-26 19:37:57 +1200

    Added slider template and db connection

---
+++ src/components/widgets/NewsSlider/slider_templates.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SliderTemplates = (props) => {
+
+    console.log(props)
+
+    return(
+        <div>
+            Slider from template
+        </div>
+    )
+}
+
+export default SliderTemplates;
