78a32b936f7de7933456d432371f7283ba0a74f2
AvBorikDeveloper <avboris28@gmail.com>
2019-04-26 20:33:31 +1200

    added image, title and setup the slider

--- src/components/widgets/NewsSlider/slider_templates.js
+++ src/components/widgets/NewsSlider/slider_templates.js
@@ -1,13 +1,53 @@
-import React from "react";
+import React from 'react';
+import styles from './slider.module.css';
+import {Link} from 'react-router-dom';
+
+import Slick from 'react-slick';
 
 const SliderTemplates = (props) => {
 
-    console.log(props)
+    let template = null;
+
+    const settings = {
+        dots:true,
+        infinite:true,
+        arrows:false,
+        speed:500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    }
+
+    switch(props.type){
+        case('featured'):
+            template = props.data.map((item,i) =>{
+                return(
+                    <div key={i}>
+                        <div className={styles.featured_item}>
+                        <div className={styles.featured_image}
+                                style={{
+                                    background: `url(../images/articles/${item.image})`
+                                }}>
+                        </div>
+                        <Link to={`/articles/${item.id}`}>
+                                <div className={styles.featured_caption}>
+                                    {item.title}
+                                </div>
+                        </Link>
+                        </div>
+                    </div>
+                )
+            })
+
+            break;
+        default:
+        template = null;
+
+    }
 
     return(
-        <div>
-            Slider from template
-        </div>
+        <Slick {...settings}>
+            {template}
+        </Slick>
     )
 }
 
